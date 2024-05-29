c82d59010d51f2b8450b4924ea7d7165b51a77f5
chakrireddy1609 <chakrireddy1609@gmail.com>
2024-05-29 13:30:11 +0530

    page title

--- Tests/test_LoginLogout.py
+++ Tests/test_LoginLogout.py
@@ -6,10 +6,16 @@ def test_loginlogout(page):
     lp.navigate()
     lp.login_logout()
 
-def test_invalidLogin(page):
+
+def test_invalidlogin(page):
     lp1 = LoginPage(page)
     lp1.navigate()
     lp1.message_invalid_login()
+
+def pagetitle(page):
+    lp2 = LoginPage(page)
+    lp2.navigate()
+    lp2.assert_page_title()
 
 
 
